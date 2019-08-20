0853c2a60ec6d5cd8180ed10c5b6357414951a70
marcelo <bernardes@leucotron.com.br>
2019-08-20 16:17:29 -0300

    - Criação do grupo de projetos com database e restapi juntos

---
+++ aula10/Readme.txt
@@ -0,0 +1,4 @@
+Notas da aula 10
+- Criando o grupo de projetos com o banco de dados e a aplicação rest criada anteriormente
+- Adicionando a dependência no pom para utilizar injeção de dependências do spring-context 
+- Injetando o DAO na aplicação rest
---
+++ aula10/sgh-database/src/main/java/br/fai/lds/sgh/database/dao/IRoomDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.fai.lds.sgh.database.dao;
+
+import br.fai.lds.sgh.database.entity.Room;
+import java.util.List;
+
+/**
+ *
+ * @author Marcelo
+ */
+public interface IRoomDao {
+    
+    public void create(Room room);
+    
+    public List<Room> readAll();
+
+    public Room readById(long id);
+
+    public void update(Room room);
+
+    public void delete(Room room);
+    
+}
